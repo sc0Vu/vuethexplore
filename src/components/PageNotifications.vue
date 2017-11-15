40a5d453caccc28111d15f711c6b190e10002fbf
sc0Vu <alk03073135@gmail.com>
2017-11-15 12:57:56 +0800

    PageNotifications.vue and spec test.

---
+++ src/components/PageNotifications.vue
@@ -0,0 +1,43 @@
+<style scoped>
+.notifications {
+  max-width: 400px;
+  min-width: 240px;
+  width: 40%;
+  position: fixed;
+  left: 5px;
+  bottom: 5px;
+}
+
+.notification {
+  width: 100%;
+  margin-bottom: 5px;
+}
+</style>
+
+<template>
+<div class="notifications">
+	<div class="notification" v-for="(notification, index) in notifications" v-bind:key="index" v-bind:class="notification.class">
+	  <button class="delete" v-on:click="remove(index)"></button>
+	  {{ notification.text }}
+	</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'page-notifications',
+  props: {
+    notifications: {
+      type: Array,
+      default () {
+        return [];
+      },
+    },
+  },
+  methods: {
+    remove (index) {
+      this.notifications.splice(index, 1);
+    },
+  },
+};
+</script>
---
+++ test/unit/specs/PageNotifications.spec.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import PageNotifications from '@/components/PageNotifications';
+
+describe('PageNotifications.vue', () => {
+  const Constructor = Vue.extend(PageNotifications);
+
+  it('should have name page-notifications', () => {
+    expect(PageNotifications.name).to.equal('page-notifications');
+  });
+
+  it('should have default props', () => {
+    const props = PageNotifications.props;
+
+    expect(props.notifications.default()).to.deep.equal([]);
+  });
+
+  it('should have method remove', () => {
+    expect(typeof PageNotifications.methods.remove).to.equal('function');
+  });
+});
