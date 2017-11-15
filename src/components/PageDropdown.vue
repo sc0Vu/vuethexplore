b029589e7a2977dacaf176f0bb4dfa0b0edcf7a2
sc0Vu <alk03073135@gmail.com>
2017-11-15 12:53:33 +0800

    PageDropdown.vue and spec test.

---
+++ src/components/PageDropdown.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="dropdown" v-bind:class="{'is-active': isShow}">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu" v-on:click="toggleDropdown">
+      <span>{{ buttonText }}</span>
+      <span class="icon is-small">
+        <i class="fa fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu" role="menu">
+    <div class="dropdown-content">
+      <a class="dropdown-item" v-for="(item, index) in items" v-bind:key="index" v-bind:data-to="item.to" v-on:click="itemClicked">
+        {{ item.text }}
+      </a>
+      <hr class="dropdown-divider">
+      <input class="input" placeholder="Enter other host" v-on:change="inputChanged">
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'page-dropdown',
+  props: {
+    buttonText: {
+      type: String,
+      default () {
+        return '------';
+      },
+    },
+    items: {
+      type: Array,
+      default () {
+        return [];
+      },
+    },
+  },
+  data () {
+    return {
+      isShow: false,
+    };
+  },
+  methods: {
+    toggleDropdown () {
+      this.isShow = this.isShow !== true;
+      this.$emit('toggle', this.isShow);
+    },
+    itemClicked (e) {
+      this.$emit('itemclick', e.target);
+    },
+    inputChanged (e) {
+      this.$emit('inputChanged', e.target);
+    },
+  },
+};
+</script>
---
+++ test/unit/specs/PageDropdown.spec.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import PageDropdown from '@/components/PageDropdown';
+
+describe('PageDropdown.vue', () => {
+  const Constructor = Vue.extend(PageDropdown);
+
+  it('should have name page-dropdown', () => {
+    expect(PageDropdown.name).to.equal('page-dropdown');
+  });
+
+  it('should have default props', () => {
+    const props = PageDropdown.props;
+
+    expect(props.buttonText.default()).to.equal('------');
+    expect(props.items.default()).to.deep.equal([]);
+  });
+
+  it('should have default data', () => {
+    const data = PageDropdown.data();
+
+    expect(data.isShow).to.equal(false);
+  });
+
+  it('should have method toggleDropdown', () => {
+    expect(typeof PageDropdown.methods.toggleDropdown).to.equal('function');
+  });
+
+  it('should have method itemClicked', () => {
+    expect(typeof PageDropdown.methods.itemClicked).to.equal('function');
+  });
+
+  it('should have method inputChanged', () => {
+    expect(typeof PageDropdown.methods.inputChanged).to.equal('function');
+  });
+});
