33c762944ae32050d2bb08f94d6e36bb6b489777
sc0Vu <alk03073135@gmail.com>
2017-11-20 17:32:01 +0800

    Add PageNumberInput.vue and unit test.

---
+++ src/components/PageNumberInput.vue
@@ -0,0 +1,34 @@
+<template>
+<input type="text" v-bind:class="inputClass" v-bind:placeholder="inputPlaceholder" v-bind:value="inputValue" v-on:change="update(inputValue)">
+</template>
+
+<script>
+export default {
+  name: 'page-number-input',
+  props: {
+    inputClass: {
+      type: String,
+      default () {
+        return 'input';
+      },
+    },
+    inputPlaceholder: {
+      type: String,
+      default () {
+        return '';
+      },
+    },
+    inputValue: {
+      type: String,
+      default () {
+        return '';
+      },
+    },
+  },
+  methods: {
+    update (value) {
+      this.inputValue = parseInt(value, 10);
+    },
+  },
+};
+</script>
---
+++ test/unit/specs/PageNumberInput.spec.js
@@ -0,0 +1,22 @@
+// import Vue from 'vue';
+import PageNumberInput from '@/components/PageNumberInput';
+
+describe('PageNumberInput.vue', () => {
+  // const Constructor = Vue.extend(PageNumberInput);
+
+  it('should have name page-number-input', () => {
+    expect(PageNumberInput.name).to.equal('page-number-input');
+  });
+
+  it('should have default props', () => {
+    const props = PageNumberInput.props;
+
+    expect(props.inputClass.default()).to.equal('input');
+    expect(props.inputPlaceholder.default()).to.equal('');
+    expect(props.inputValue.default()).to.equal('');
+  });
+
+  it('should have update', () => {
+    expect(typeof PageNumberInput.methods.update, 'function');
+  });
+});
