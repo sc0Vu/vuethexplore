daf06c9f70b1bc451c2dcd736221848e1c02e360
sc0Vu <alk03073135@gmail.com>
2017-11-22 15:30:13 +0800

    Add tab in Block.vue.

--- src/views/Block.vue
+++ src/views/Block.vue
@@ -1,32 +1,49 @@
 <template>
-<div class="section">
+<div>
   <div v-if="!connected">Please choose the host to connect blockchain! </div>
   <div v-if="connected && loading">Loading! </div>
   <div v-if="connected && !loading">
-    <div class="columns">
+    <div class="section">
       <div class="card">
         <header class="card-header">
           <p class="card-header-title">
             Block #{{ block.number }}
           </p>
+          <div class="tabs">
+            <ul>
+              <li v-bind:class="{ 'is-active': isSelected('information') }">
+                <a v-on:click="select('information')">Information</a>
+              </li>
+              <li v-bind:class="{ 'is-active': isSelected('transactions') }">
+                <a v-on:click="select('transactions')">Transactions</a>
+              </li>
+            </ul>
+          </div>
         </header>
         <div class="card-content">
           <div class="content" style="word-wrap: break-word;">
-            <p>Transaction: <router-link v-bind:to="{ name: 'Transaction', params: { transactionHash: block.transactions[0] } }">{{ block.transactions[0] }}</router-link>, and other {{block.transactions.length - 1 }} transactions</p>
-            <p>Difficulty {{ block.difficulty }}</p>
-            <p>Total Difficulty: {{ block.totalDifficulty }}</p>
-            <p>Gas Limit: {{ block.gasLimit }}</p>
-            <p>Gas Used: {{ block.gasUsed }}</p>
-            <p>Hash: {{ block.hash }}</p>
-            <p>Parent Hash: <router-link v-bind:to="{ name: 'Block', params: { blockNumber: block.parentHash } }">{{ block.parentHash }}</router-link></p>
-            <p>Logs Bloom: {{ block.logsBloom }}</p>
-            <p>SHA3 Uncles: {{ block.sha3Uncles }}</p>
-            <p>Miner: {{ block.miner }}</p>
-            <p>Mix Hash: {{ block.mixHash }}</p>
-            <p>Nonce: {{ block.nonce }}</p>
-            <p>Size: {{ block.size }}</p>
-            <p>Extra Data: {{ toUtf8(block.extraData) }} (Hex: {{ block.extraData }})</p>
-            <time>Timestamp: {{ block.timestamp }} ({{ new Date(block.timestamp * 1000).toString() }})</time>
+            <div v-show="isSelected('information')">
+              <p>Transaction: <router-link v-bind:to="{ name: 'Transaction', params: { transactionHash: block.transactions[0] } }">{{ block.transactions[0] }}</router-link>, and other {{block.transactions.length - 1 }} transactions</p>
+              <p>Difficulty {{ block.difficulty }}</p>
+              <p>Total Difficulty: {{ block.totalDifficulty }}</p>
+              <p>Gas Limit: {{ block.gasLimit }}</p>
+              <p>Gas Used: {{ block.gasUsed }}</p>
+              <p>Hash: {{ block.hash }}</p>
+              <p>Parent Hash: <router-link v-bind:to="{ name: 'Block', params: { blockNumber: block.parentHash } }">{{ block.parentHash }}</router-link></p>
+              <p>Logs Bloom: {{ block.logsBloom }}</p>
+              <p>SHA3 Uncles: {{ block.sha3Uncles }}</p>
+              <p>Miner: {{ block.miner }}</p>
+              <p>Mix Hash: {{ block.mixHash }}</p>
+              <p>Nonce: {{ block.nonce }}</p>
+              <p>Size: {{ block.size }}</p>
+              <p>Extra Data: {{ toUtf8(block.extraData) }} (Hex: {{ block.extraData }})</p>
+              <time>Timestamp: {{ block.timestamp }} ({{ new Date(block.timestamp * 1000).toString() }})</time>
+            </div>
+            <div v-show="isSelected('transactions')">
+              <p v-for="transaction in block.transactions">
+                <router-link v-bind:to="{ name: 'Transaction', params: { transactionHash: transaction } }">{{ transaction }}</router-link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -44,6 +61,7 @@ export default {
     return {
       block: {},
       loading: true,
+      selected: '',
     };
   },
   computed: {
@@ -89,11 +107,25 @@ export default {
       }).catch((err) => {
         this.notify({ text: `Failed to get block ${blockNumber}! Error: ${err.message}`, class: 'is-danger' });
       }).then(() => {
+        this.select('information');
         this.loading = false;
       });
     },
     toUtf8 (hex) {
       return this.web3.utils.toUtf8(hex);
+    },
+    isSelected (tab) {
+      if (/^[\w]+$/.test(tab) === false) {
+        this.notify({ text: `Cannot check ${tab} is selected!`, class: 'is-danger' });
+        return false;
+      }
+      return tab === this.selected;
+    },
+    select (tab) {
+      if (/^[\w]+$/.test(tab) === false) {
+        this.notify({ text: `Cannot select ${tab}!`, class: 'is-danger' });
+      }
+      this.selected = tab;
     },
   },
   watch: {
