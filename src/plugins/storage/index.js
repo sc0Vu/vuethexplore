a3de49b63207b52184ca8d8bfc5ee059046fc2e4
sc0Vu <alk03073135@gmail.com>
2018-03-02 14:49:33 +0800

    storage plugin

---
+++ src/plugins/storage/index.js
@@ -0,0 +1,43 @@
+const storagePlugun = {};
+
+storagePlugun.install = function(Vue, options) {
+  const opts = Object.assign({
+    module: 'localStorage',
+  }, options);
+
+  if (opts.module !== 'localStorage') {
+    throw new Error('Sorry we only support localStorage now.');
+  }
+
+  const storage = {
+    isLocalStorage () {
+      if (!window) {
+        return false;
+      }
+      return window.localStorage !== undefined && opts.module === 'localStorage';
+    },
+    getItem (key) {
+      if (this.isLocalStorage()) {
+        return window.localStorage.getItem(key);
+      }
+      return false;
+    },
+    setItem (key, value) {
+      if (this.isLocalStorage()) {
+        window.localStorage.setItem(key, value);
+      }
+    },
+    isExist (key) {
+      // if value is boolean false, it return string false
+      const item = this.getItem(key);
+
+      return item !== false && item !== undefined && item !== null;
+    },
+  };
+
+  Object.defineProperty(Vue.prototype, '$storage', {
+    get () { return storage; },
+  });
+};
+
+export default storagePlugun;
