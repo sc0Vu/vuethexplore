832ac8d521b9bf690602f66374d4b19323668e7c
sc0Vu <alk03073135@gmail.com>
2018-03-02 16:34:50 +0800

    Add transaction logs view.

--- src/views/Transaction.vue
+++ src/views/Transaction.vue
@@ -19,7 +19,28 @@
             <p>To: <router-link v-bind:to="{ name: 'Address', params: { address: transaction.to } }">{{ transaction.to }}</router-link></p>
             <p>Status: {{ transaction.status }}</p>
             <p>Contract Address: {{ (transaction.contractAddress) ? transaction.contractAddress : 'Not contract transaction.' }}</p>
-            <p>Logs: <span v-html="logs"></span></p>
+            <p>Logs: 
+              <template v-if="logs !== false">
+                <ul>
+                  <li v-for="(log, index) in logs" v-on:click="selectLog(log)"><code>log - {{ index }}</code></li>
+                </ul>
+                <template v-if="selectedLog.address">
+                  <p style="background-color: whitesmoke;padding: 1.25em 1.5em;word-wrap: normal;overflow-x:scroll;">
+                    Selected log: {{ selectedLog.logIndex }}<br>
+                    Address: {{ selectedLog.address }}<br>
+                    Block Hash: <router-link v-bind:to="{ name: 'Block', params: { blockNumber: selectedLog.blockHash } }">{{ selectedLog.blockHash }}</router-link><br>
+                    Block Number: <router-link v-bind:to="{ name: 'Block', params: { blockNumber: selectedLog.blockNumber } }">{{ selectedLog.blockNumber 
+                  }}</router-link><br>
+                    Data: {{ selectedLog.data }}<br>
+                    Removed: {{ selectedLog.removed }}<br>
+                    Topics: {{ selectedLog.topics }}<br>
+                    Transaction Hash: {{ selectedLog.transactionHash }}<br>
+                    Transaction Index: {{ selectedLog.transactionIndex }}<br>
+                  </p>
+                </template>
+              </template>
+              <span v-else>No logs.</span>
+            </p>
             <p>Logs Bloom: {{ transaction.logsBloom }}</p>
             <p>Cumulative Gas Used: {{ transaction.cumulativeGasUsed }}</p>
             <p>Gas Used: {{ transaction.cumulativeGasUsed }}</p>
@@ -40,6 +61,7 @@ export default {
     return {
       transaction: {},
       loading: true,
+      selectedLog: {},
     };
   },
   computed: {
@@ -55,9 +77,9 @@ export default {
       const transaction = this.transaction;
 
       if (transaction.logs) {
-        return (transaction.logs.length > 0) ? transaction.logs.join('<br>') : 'No logs.';
+        return (transaction.logs.length > 0) ? transaction.logs : false;
       }
-      return '';
+      return false;
     },
   },
   created () {
@@ -86,6 +108,13 @@ export default {
       }).then(() => {
         this.loading = false;
       });
+    },
+    selectLog (log) {
+      if (log.address) {
+        this.selectedLog = log;
+      } else {
+        this.notify({ text: 'Please select correst log.', class: 'is-danger' });
+      }
     },
   },
 };
