27bde3ea3538e93e018a36535f3b860743451e80
sc0vu <alk03073135@gmail.com>
2019-04-19 14:37:33 +0800

    Remove notification after 2 seconds

--- test/unit/specs/StatePage.spec.js
+++ test/unit/specs/StatePage.spec.js
@@ -25,4 +25,21 @@ describe('State page', () => {
 
     expect(store.state.page.loading).to.equal(false);
   });
+
+  it('should set autoRemove to false', () => {
+    expect(store.state.page.autoRemove).to.equal(true);
+
+    page.mutations.setAutoRemove(store.state.page, false);
+
+    expect(store.state.page.autoRemove).to.equal(false);
+  });
+
+  it('should set timeout to 1000', () => {
+    // default is 2000
+    expect(store.state.page.timeout).to.equal(2000);
+
+    page.mutations.setTimeout(store.state.page, 1500);
+
+    expect(store.state.page.timeout).to.equal(1500);
+  });
 });
